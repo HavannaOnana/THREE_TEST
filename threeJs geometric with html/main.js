2bcd0497846b3c0f059c2215b53f9dd3d7338263
HavannaOnana <138626351+HavannaOnana@users.noreply.github.com>
2024-07-01 09:19:05 +0200

    done with the earth just need to add some adjuestments then done

--- threeJs geometric with html/main.js
+++ threeJs geometric with html/main.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 import './style.css';  // Make sure style.css exists if you're importing it
+import { getFresnelMat } from '../getFresnelMat';
+
 
 // We are making a renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -20,37 +22,35 @@ camera.position.z = 2;
 // Making a new scene 
 const scene = new THREE.Scene();
 
+//adding a loader
+const loader = new THREE.TextureLoader();
+
 // Adding the geometry and material
 const geometry = new THREE.IcosahedronGeometry(1, 20);
 const material = new THREE.MeshStandardMaterial({
   color: 0xffffff,
+  map : loader.load('/textures/earthy.jpg')
 });
 
-const loader = new THREE.TextureLoader();
-const texture = loader.load('/textures/earthy.jpg', 
-  function ( texture ) {
-    // onLoad callback
-    material.map = texture;
-    material.needsUpdate = true;
-  },
-  undefined, // onProgress callback, currently not used
-  function ( err ) {
-    console.error('An error happened while loading the texture.');
-  }
-);
+//making the earthgroup
+const earthGroup = new THREE.Group();
+scene.add(earthGroup);
+
 
 
 // Adding a mesh
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const EarthMesh = new THREE.Mesh(geometry, material);
+earthGroup.rotation.z = -23.4 * Math.PI / 180;
+earthGroup.add(EarthMesh);
 
 // Adding a light
-const hemiLight = new THREE.HemisphereLight("black", "white");
+const hemiLight = new THREE.HemisphereLight("black", "lightblue");
 scene.add(hemiLight);
 
 // making something different in the controls
 const controls = new OrbitControls(camera,renderer.domElement);
 controls.enableZoom = false;
+controls.enableRotate = false;
 
 // this event listner prevent us from zooming in
 window.addEventListener('wheel', (event) => {
@@ -61,14 +61,44 @@ window.addEventListener('wheel', (event) => {
 
 //adding sunlight
 const Sunlight = new THREE.DirectionalLight("white");
-Sunlight.position.set(-12,-0.5,1.5);
+Sunlight.position.set(-2,0.5,1.5);
 scene.add(Sunlight);
+
+// code for making the light on earth
+const lightsMat = new THREE.MeshBasicMaterial({
+  map : loader.load('/textures/earthdark.png'),
+  blending : THREE.AdditiveBlending,
+  transparent: true,
+})
+
+const lightsMesh = new THREE.Mesh(geometry,lightsMat);
+earthGroup.add(lightsMesh);
+
+//adding clouds too
+const cloudMat = new THREE.MeshBasicMaterial({
+  map : loader.load('/textures/clouds.jpg'),
+  blending : THREE.AdditiveBlending
+})
+
+const cloudMesh = new THREE.Mesh(geometry,cloudMat);
+cloudMesh.scale.setScalar(1.003);
+earthGroup.add(cloudMesh);
+
+//making a new mesh for the glow
+const fresnelMat = getFresnelMat();
+const glowMesh = new THREE.Mesh(geometry,fresnelMat);
+glowMesh.scale.setScalar(1.01);
+earthGroup.add(glowMesh);
+
 
 
 // Animation loop
 function animate() {
   requestAnimationFrame(animate);
-  mesh.rotation.y += 0.0008;
+  EarthMesh.rotation.y += 0.0008;
+  lightsMesh.rotation.y += 0.0008;
+  cloudMesh.rotation.y += 0.001;
+  glowMesh.rotation.y += 0.0008;
   renderer.render(scene, camera);
 }
 
